eb72f8881aabef18d7f85cd641b6d1f650509c53
leigh m aucoin <lmaucoin@gmail.com>
2015-11-30 15:54:24 -0600

    testing.

---
+++ test-imgur.php
@@ -0,0 +1,6 @@
+<?php
+$uploader = ChipVN_ImageUploader_Manager::make('Imgur');
+$uploader->setApi('959b5e4667b3d62');
+$uploader->setSecret('9709065b895d1ab8d92d88c380a7a4315d0e6ed9');
+echo $uploader->upload(getcwd(). '/treasure.gif');
+?>
