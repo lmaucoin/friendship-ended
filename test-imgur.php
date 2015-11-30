d9585da6051b20f310bdf7a9b24abb1e3a1e5d4f
leigh m aucoin <lmaucoin@gmail.com>
2015-11-30 15:57:38 -0600

    testing.

--- test-imgur.php
+++ test-imgur.php
@@ -1,4 +1,6 @@
 <?php
+include getcwd().'vendor/ptcong/php-image-uploader/ChipVN/ClassLoader/Loader.php';
+ChipVN_ClassLoader_Loader::registerAutoLoad();
 $uploader = ChipVN_ImageUploader_Manager::make('Imgur');
 $uploader->setApi('959b5e4667b3d62');
 $uploader->setSecret('9709065b895d1ab8d92d88c380a7a4315d0e6ed9');
